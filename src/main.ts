3e4e205cfb303dd65acadabcd104a45d0845acd5
Diego Reyes <diegorele13@gmail.com>
2025-09-20 13:38:27 -0500

    refactor: move global styles import to top and switch to scss
    Use scss for global styles instead of css for better maintainability and features

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import '@/styles/global.scss'
 
 import App from './App.vue'
 import router from './router'
